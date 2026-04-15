c35957cc33bc82e4b03306991513a8377b1ca5b5
workntones-cyber <work.ntones@gmail.com>
2026-04-15 10:43:06 +0900

    bug fix!

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -38,7 +38,7 @@ namespace AnimePlayer
                     });
                 };
                 InitializeTimer();
-                _mediaPlayer.Volume = (int)VolumeBar.Value;
+                _mediaPlayer.Volume = 100;
             }
             catch (Exception ex)
             {
